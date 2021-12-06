62e1a28c54c0722a4cf8bda5ff273787a444cee4
marinzilic7 <marinzilic507@gmail.com>
2021-12-06 01:41:49 +0100

    prvi commit

---
+++ zadaca.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel = "stylesheet" href="style.css">
+    <title>Zadaca</title>
+</head>
+<body>
+    <div class="vizija">
+        <button><a href="https://docs.google.com/document/d/1mXK2ZZGDE5Wix-8ZRMAld20EvYWbr2VUNg6q7dknbkg/edit">Vizija</a></button>
+    </div>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <div class="zivotopis">
+        <h1> Životopis</h1>
+    </div>
+
+    <div class="img-info">
+        
+    <img src="nare.jpeg.jpeg" alt ="zyla"height = "250px" width="210px">
+    <img src="zyla.jpeg.jpeg" alt="nare" height = "250px" width="210px">
+    
+    </div>
+
+    <br>
+    
+    <div class="names">
+        <h1>Josip Kordić</h1>
+        <h1>Marin Žilić</h1>
+    </div>
+    <div class="opis">
+        
+        <p id="nare-opis">Student sam 3. godine Informatike na FPMOZU, imam 21 godinu, dolazim iz Ljubuškog. Cilj mi je nauciti backend i uspijeti u karijeri. </p>
+        <p id="zyla-opis">Zdravo , student sam 3. godine studija Informatike na FPMOZU u Mostaru.Imam 23 godine i dolazim iz Ljubuškog.Kolegu poznajem od prve godine faksa 
+            , ja sam vise fokusiran na Frontend dio dok je kolega na backend. Nadamo se da cemo uspijeti odraditi ovaj projekt onako kako smo zamislili.
+
+        </p>
+    </div>
+    <br>
+    <br>
+    
+    <hr>
+
+    <br>
+    <br>
+    <div class="o-projektu">
+        <button>Projekt</button>
+        
+
+    </div>
+    <br>
+    <br>
+
+    <div class = "oPis">
+        <p>Cilj nam je napraviti Gaming shop u kojem ce korisnici moci da pretraze, pogledaju ili kupe artikle koji budu na raspologanju ili koji im se budu svidjeli. 
+        Pri učitavanju stranice učitat ce se register / login forma gdje ce korisnik morati da se registrira odnosno prijavi kako bih pristupio glavnoj stranici te kupio sta odluci uz naravno gosta koji ce imati pristup stranici ali bez mogucnost kupnje.
+        Kada korisnik udje na stranicu imat ce na raspolaganju pogled na sve dostupne artikle, uz to ce imati mogucnost da pretrazi artikl koji zeli, doda u kosaricu , stavi u uzi izbor. 
+        Na stranicu mogucnost prijave imaju administratori koji ce moci da uredjuju artikle, dodavaju nove artikle, brišu artikle.
+        Običan korisnik ce imati mogucnost da kupi artikl, dodavat u uži izbor ili u stvari koje mu se svidjaju. 
+        Gost ce imat pristup stranici ali nece imati nikakvu drugu mogucnost na stranici ( kupovanje, dodavanje u stvari koje mu se svidjaju)</p>
+     </div>
+     <br>
+     <br>
+     <br>
+
+     <hr>
+     <br>
+     <br>
+     
+     <div class="tehnologije">
+         <button>Tehnologije</button>
+     </div>
+
+     <br>
+     <br>
+     <br>   
+     
+
+     <div class="slike-t">
+         <img id ="html"src="https://i.stack.imgur.com/PgcSR.png" alt="" height="180px" width="180px">
+         <img id="css"src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png" alt="" height="180px" width="140px">
+         <img src="https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg" height="150px" width="180px">
+         <img src="http://assets.stickpng.com/thumbs/58482acecef1014c0b5e4a1e.png" height="150px" width="180px">
+         <img src="https://download.logo.wine/logo/MySQL/MySQL-Logo.wine.png" height="150px" width="180px">
+         <img src="https://cdn.freebiesupply.com/logos/large/2x/php-1-logo-png-transparent.png" height="130px" width="200px">
+         <img src="https://download.logo.wine/logo/Bootstrap_(front-end_framework)/Bootstrap_(front-end_framework)-Logo.wine.png" height="160px" width="230px">
+
+     </div>
+        <br>
+        <br>
+        <br>
+     <hr>
+
+     <br>
+     <br>
+
+     <div class="motivacija">
+         <button>Motivacija</button>
+     </div>
+
+     <br>
+     <br>
+
+     <div class="opis-m">
+         <p>Zanima nas web programiranje i web tehnologije, s ovim projektom imamo priliku naucit neke nove stvari. Rad u timu nam daje jos dodatni motiv naprijed.
+             Svjesni smo da ce nam ovaj projekt unaprijediti nase znanje te nas motivirati u nasem daljem radu.
+         </p>
+     </div>
+
+
+    
+    
+
+    
+
+    
+    
+
+</body>
+</html>
